8c691b77098ba2a9c5e44d88da0d2a360c0f4aaf
Mariam Gasparian <m.gasparian11@gmail.com>
2024-02-26 22:49:45 +0400

    one example implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NormalInputComponent } from './normal-input/normal-input.component';
+import { SignalInputComponent } from './signal-input/signal-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignalInputComponent, NormalInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-signal-inputs';
+  count = 0;
+
+  increaseCount(): void {
+    this.count++;
+  }
 }
